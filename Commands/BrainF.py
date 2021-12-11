06284cc43f97fc946823716d3b2e056adb4406e0
Ryan Peabody <rpeabody43@gmail.com>
2021-12-11 00:09:05 -0500

    added bf

---
+++ Commands/BrainF.py
@@ -0,0 +1,95 @@
+import discord
+from discord.ext import commands
+
+# arg must be a string
+
+
+def brainf_string(arg):
+    try:
+        turing_machine = [0] * 100
+        pointer = 0
+        chars = []
+        i = 0
+
+        while i < len(arg):
+            # if turing_machine[pointer] == None:
+            #     turing_machine[pointer] = 0
+
+            if turing_machine[pointer] > 255 or turing_machine[pointer] < 0:
+                raise ValueError("Only int values between 0 and 255")
+
+            elif arg[i] == '<':
+                if pointer <= 0:
+                    raise MemoryError
+                pointer -= 1
+
+            elif arg[i] == '>':
+                pointer += 1
+
+            elif arg[i] == '+':
+                turing_machine[pointer] += 1
+
+            elif arg[i] == '-':
+                if turing_machine[pointer] > 0:
+                    turing_machine[pointer] -= 1
+
+            elif arg[i] == '.':
+                chars.append(turing_machine[pointer])
+
+            elif arg[i] == '[':
+                if turing_machine[pointer] == 0:
+                    loop = 1
+                    while loop > 0:
+                        i += 1
+                        if arg[i] == '[':
+                            loop += 1
+                        elif arg[i] == ']':
+                            loop -= 1
+
+            elif arg[i] == ']':
+                loop = 1
+                while loop > 0:
+                    i -= 1
+                    if arg[i] == '[':
+                        loop -= 1
+                    elif arg[i] == ']':
+                        loop += 1
+                i -= 1
+
+            i += 1
+
+        return_str = ""
+        for i in chars:
+            return_str += chr(i)
+        # return_str = chars
+        return return_str
+    except:
+        print("it's here you dumbass")
+
+
+class BrainFCog (commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def bf(self, ctx, *, arg):
+        print(arg)
+        await ctx.reply('`' + brainf_string(arg) + '`')
+
+    @bf.error
+    async def bf_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('$bf (brainf code)')
+
+        if isinstance(error, MemoryError):
+            await ctx.send('*`Failed to compile: Memory Error`*')
+        if isinstance(error, ValueError):
+            await ctx.send('*`Failed to compile: Value Error`*')
+
+        else:
+            await ctx.send('*`Failed to compile`*')
+        print("", str(ctx.message.jump_url), str(error), sep='\n')
+
+
+def setup(bot):
+    bot.add_cog(BrainFCog(bot))
--- Main.py
+++ Main.py
@@ -1,22 +1,28 @@
 
-import discord                      #boy I wonder what this is
-from discord.ext import commands    #commands module of the discord api
-import os                           #open .env files
+import discord  # boy I wonder what this is
+from discord.ext import commands  # commands module of the discord api
+import os  # open .env files
 from dotenv import load_dotenv
 
-bot = commands.Bot(command_prefix="$") #creating a bot instance, the connection to discord
+# creating a bot instance, the connection to discord
+bot = commands.Bot(command_prefix='$')
 bot.load_extension('Commands.TwitchIntegration')
-        
+bot.load_extension('Commands.BrainF')
+
 load_dotenv()
 
-#@bot.event is used to register an event
+# @bot.event is used to register an event
+
+
 @bot.event
-#on_ready is called when the bot is ready to be used (good for initialization)
+# on_ready is called when the bot is ready to be used (good for initialization)
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
 
-def main():
-    bot.run(os.getenv('DiscordToken')) #test comment funny haha
 
-if __name__  == '__main__':
+def main():
+    bot.run(os.getenv('DiscordToken'))  # test comment funny haha
+
+
+if __name__ == '__main__':
     main()
