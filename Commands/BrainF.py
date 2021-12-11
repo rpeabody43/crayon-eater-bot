a2468112eb3bf38bdc7e8323937e6b41d8b7eabc
Ryan Peabody <rpeabody43@gmail.com>
2021-12-11 02:36:19 -0500

    bf formatting

--- Commands/BrainF.py
+++ Commands/BrainF.py
@@ -5,66 +5,60 @@ from discord.ext import commands
 
 
 def brainf_string(arg):
-    try:
-        turing_machine = [0] * 100
-        pointer = 0
-        chars = []
-        i = 0
+    turing_machine = [0] * 100
+    pointer = 0
+    chars = []
+    i = 0
 
-        while i < len(arg):
-            # if turing_machine[pointer] == None:
-            #     turing_machine[pointer] = 0
+    while i < len(arg):
+        if turing_machine[pointer] > 255 or turing_machine[pointer] < 0:
+            raise ValueError("Only int values between 0 and 255")
 
-            if turing_machine[pointer] > 255 or turing_machine[pointer] < 0:
-                raise ValueError("Only int values between 0 and 255")
+        elif arg[i] == '<':
+            if pointer <= 0:
+                raise MemoryError
+            pointer -= 1
 
-            elif arg[i] == '<':
-                if pointer <= 0:
-                    raise MemoryError
-                pointer -= 1
+        elif arg[i] == '>':
+            pointer += 1
 
-            elif arg[i] == '>':
-                pointer += 1
+        elif arg[i] == '+':
+            turing_machine[pointer] += 1
 
-            elif arg[i] == '+':
-                turing_machine[pointer] += 1
+        elif arg[i] == '-':
+            if turing_machine[pointer] > 0:
+                turing_machine[pointer] -= 1
 
-            elif arg[i] == '-':
-                if turing_machine[pointer] > 0:
-                    turing_machine[pointer] -= 1
+        elif arg[i] == '.':
+            chars.append(turing_machine[pointer])
 
-            elif arg[i] == '.':
-                chars.append(turing_machine[pointer])
-
-            elif arg[i] == '[':
-                if turing_machine[pointer] == 0:
-                    loop = 1
-                    while loop > 0:
-                        i += 1
-                        if arg[i] == '[':
-                            loop += 1
-                        elif arg[i] == ']':
-                            loop -= 1
-
-            elif arg[i] == ']':
+        elif arg[i] == '[':
+            if turing_machine[pointer] == 0:
                 loop = 1
                 while loop > 0:
-                    i -= 1
+                    i += 1
                     if arg[i] == '[':
-                        loop -= 1
-                    elif arg[i] == ']':
                         loop += 1
+                    elif arg[i] == ']':
+                        loop -= 1
+
+        elif arg[i] == ']':
+            loop = 1
+            while loop > 0:
                 i -= 1
+                if arg[i] == '[':
+                    loop -= 1
+                elif arg[i] == ']':
+                    loop += 1
+            i -= 1
 
-            i += 1
+        i += 1
 
-        return_str = ""
-        for i in chars:
-            return_str += chr(i)
-        # return_str = chars
-        return return_str
-    except:
-        print("it's here you dumbass")
+    return_str = ""
+    for i in chars:
+        return_str += chr(i)
+    # return_str = chars
+    return return_str
 
 
 class BrainFCog (commands.Cog):
@@ -73,18 +67,17 @@ class BrainFCog (commands.Cog):
 
     @commands.command()
     async def bf(self, ctx, *, arg):
-        print(arg)
-        await ctx.reply('`' + brainf_string(arg) + '`')
+        await ctx.reply('```fix\n' + brainf_string(arg) + '```')
 
     @bf.error
     async def bf_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('$bf (brainf code)')
+            await ctx.reply('$bf (brainf code)')
 
         if isinstance(error, MemoryError):
-            await ctx.send('*`Failed to compile: Memory Error`*')
+            await ctx.reply('*`Failed to compile: Memory Error`*')
         if isinstance(error, ValueError):
-            await ctx.send('*`Failed to compile: Value Error`*')
+            await ctx.reply('*`Failed to compile: Value Error`*')
 
         else:
             await ctx.send('*`Failed to compile`*')
