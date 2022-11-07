adba8053edb34c7008cd6a0ec4518167000ce7af
Ryan Peabody <rpeabody43@gmail.com>
2022-11-06 22:11:49 -0500

    rename old-hall package, add refresh script

---
+++ refresh_leaderboard.py
@@ -0,0 +1,9 @@
+# This script is kinda pointless 
+# but it's the easiest way to run this script
+from commands.oldhall.leaderboard.update import update
+
+def main():
+    update()
+
+if __name__  == '__main__':
+    main()
